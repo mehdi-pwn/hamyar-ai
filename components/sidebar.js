f986a589f2858a9d58e66c7040aa3053b2aeef52
mehdi-pwn <amsahragard239@gmail.com>
2023-09-09 09:37:51 +0330

    sidebar full

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,11 +1,114 @@
 import { motion } from "framer-motion";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { FaShopware } from "react-icons/fa";
 import { MdOutlineCancel } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
+
+const itemClass =
+  "flex items-center gap-2 text-white rounded-md cursor-pointer font-medium hover:bg-slate-800 bg-opacity-8 px-3 py-2 mt-2";
+
+const NavItem = ({ path, icon, title }) => {
+  const { sidebarActive, setSidebarActive, screenSize } = useStateContext();
+  const handleCloseSideBar = () => {
+    if (sidebarActive && screenSize < 900) {
+      setSidebarActive(false);
+    }
+  };
+  return (
+    <li>
+      <Link
+        href={"/" + path}
+        className={itemClass}
+        onClick={handleCloseSideBar}
+      >
+        <span className="text-gray-400">{icon}</span>
+        <span className="text-white">{title}</span>
+      </Link>
+    </li>
+  );
+};
+
+const SubMenu = ({ data }) => {
+  const { sidebarActive, setSidebarActive, screenSize } = useStateContext();
+  const handleCloseSideBar = () => {
+    if (sidebarActive && screenSize < 900) {
+      setSidebarActive(false);
+    }
+  };
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const menus = data.menus;
+  return (
+    <>
+      <li
+        className="flex justify-between items-center text-white rounded-md cursor-pointer font-medium hover:bg-slate-800 bg-opacity-8 px-3 py-2 mt-2"
+        onClick={() => setSubMenuOpen(!subMenuOpen)}
+      >
+        <div className="flex items-center gap-2">
+          <span className="text-gray-400">{data.icon}</span>
+          <span className="text-white">{data.title}</span>
+        </div>
+        <IoIosArrowDown
+          className={` ${subMenuOpen && "rotate-180"} duration-200 `}
+        />
+      </li>
+      <motion.ul
+        animate={
+          subMenuOpen
+            ? {
+                height: "fit-content",
+              }
+            : {
+                height: 0,
+              }
+        }
+        className="flex pl-5 flex-col overflow-hidden"
+      >
+        {menus.map((subitem, i) => (
+          <li key={i}>
+            <Link
+              href={subitem.path}
+              className={itemClass + " mr-5 w-full"}
+              onClick={handleCloseSideBar}
+            >
+              <span className="text-gray-300">{subitem.icon}</span>
+              <span className="text-white">{subitem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </motion.ul>
+    </>
+  );
+};
 
 const Sidebar = () => {
-  const { sidebarActive, setSidebarActive } = useStateContext();
+  const { sidebarActive, setSidebarActive, screenSize, setScreenSize } =
+    useStateContext();
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setSidebarActive(false);
+    } else {
+      setSidebarActive(true);
+    }
+  }, [screenSize]);
+
+  const handleCloseSideBar = () => {
+    if (sidebarActive && screenSize < 900) {
+      setSidebarActive(false);
+    }
+  };
+
   const sidebar_animation_settings = {
     open: {
       width: "18rem",
@@ -29,7 +132,7 @@ const Sidebar = () => {
       <div className="flex justify-between items-center px-3 pb-4 border-b border-gray-800">
         <Link
           href="/"
-          onClick={() => {}}
+          onClick={handleCloseSideBar}
           className="flex flex-row text-2xl items-center font-extrabold text-white gap-3 mx-auto"
         >
           <FaShopware /> <span>میهن اِی آی</span>
@@ -44,9 +147,72 @@ const Sidebar = () => {
           <MdOutlineCancel />
         </button>
       </div>
-      ...items
+      <div className="flex flex-col h-full">
+        <ul className="px-4 py-3">
+          <NavItem
+            path={"tools"}
+            icon={<FaShopware />}
+            title={"همه ابزار ها"}
+          />
+          <div className="mt-4 mr-3 -mb-1">
+            <small className="text-slate-500">ابزار ها</small>
+          </div>
+          {sub_routes.map((item, index) => {
+            return <SubMenu key={index} data={item} />;
+          })}
+        </ul>
+      </div>
     </motion.div>
   );
 };
-
+const sub_routes = [
+  {
+    title: "ابزار های مقاله نویسی",
+    icon: <FaShopware />,
+    menus: [
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+    ],
+  },
+  {
+    title: "ابزار های مقاله نویسی",
+    icon: <FaShopware />,
+    menus: [
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+      {
+        title: "مقاله نویس",
+        icon: <FaShopware />,
+        paid: true,
+        path: "/article-writer",
+      },
+    ],
+  },
+];
 export default Sidebar;
--- contexts/ContextProvider.js
+++ contexts/ContextProvider.js
@@ -4,12 +4,15 @@ const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [sidebarActive, setSidebarActive] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
 
   return (
     <StateContext.Provider
       value={{
         sidebarActive,
         setSidebarActive,
+        screenSize,
+        setScreenSize,
       }}
     >
       {children}
