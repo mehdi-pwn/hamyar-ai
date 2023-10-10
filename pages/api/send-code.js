1333d865c0f8cf95dd4a1ad16ce8ea44e69743a8
mehdi-pwn <amsahragard239@gmail.com>
2023-10-10 16:46:03 +0330

    v1

--- pages/api/send-code.js
+++ pages/api/send-code.js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
       } catch (error) {
         return res.status(500).json({
           status: "fail",
-          error,
+          error: "catch",
         });
       }
       break;
