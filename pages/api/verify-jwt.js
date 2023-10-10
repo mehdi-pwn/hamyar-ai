132846fa9b83b547dd57dd53febbae0f35958657
mehdi-pwn <amsahragard239@gmail.com>
2023-10-10 15:59:54 +0330

    Upload v4

--- pages/api/verify-jwt.js
+++ pages/api/verify-jwt.js
@@ -10,12 +10,12 @@ export default async function handler(req, res) {
   });
 
   res.status(200).json({ status: "failed", error: "Invalid token" });
-  // const token = req.cookies.token;
+  const token = req.cookies.token;
 
-  // try {
-  //   const decoded = jwt.verify(token, process.env.AUTH_SECRET);
-  //   res.status(200).json({ status: "success", decoded });
-  // } catch (error) {
-  //   res.status(200).json({ status: "failed", error: "Invalid token" });
-  // }
+  try {
+    const decoded = jwt.verify(token, process.env.AUTH_SECRET);
+    res.status(200).json({ status: "success", decoded });
+  } catch (error) {
+    res.status(200).json({ status: "failed", error: "Invalid token" });
+  }
 }
