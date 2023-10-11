27b6d08bad9b1d4809026f71eb9174a2ade500a2
mehdi-pwn <amsahragard239@gmail.com>
2023-10-11 09:04:51 +0330

    V1

--- pages/api/verify-jwt.js
+++ pages/api/verify-jwt.js
@@ -2,14 +2,6 @@ import jwt from "jsonwebtoken";
 import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
-  await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
-
-  res.status(200).json({ status: "failed", error: "Invalid token" });
   const token = req.cookies.token;
 
   try {
